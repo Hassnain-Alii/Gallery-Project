cab66bf66e2cddf2cb1745f1b69da8cc5d6abeac
Hassnain Ali <135995911+Hassnain-Alii@users.noreply.github.com>
2026-04-06 23:07:04 +0500

    update 10

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useGallery } from '../context/GalleryContext';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
